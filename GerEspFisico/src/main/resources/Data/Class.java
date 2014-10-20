c0ccdb49236cdeb5851b488b6083213dc612cb51
EduardoJaeger <dudufjaeger@gmail.com>
2014-10-20 08:49:17 -0200

    more changes

--- GerEspFisico/src/main/resources/Data/Class.java
+++ GerEspFisico/src/main/resources/Data/Class.java
@@ -1,18 +1,17 @@
+import java.util.HashSet;
+
 
 public class Class {
-	class Period{
-		protected int weekDay;
-		protected int hour;
-		protected int minute;
-		public Peiod(int weekDay, int hour, int minute){
-			this.weekDay = weekDay;
-			this.hour = hour;
-			this.minute = minute;
-		}
-	}
 	
 	private Discipline discipline;
-	private Period period;
+	private HashSet<ClassTime> time;
 	
+	public Class (Discipline disc){
+		this.discipline = disc;
+		time = new HashSet<ClassTime>();
+	}
 	
+	public Discipline getDiscipline(){
+		return discipline;
+	}
 }
