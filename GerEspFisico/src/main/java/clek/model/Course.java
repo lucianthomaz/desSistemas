d94967a3812922a050bee59a11159b4670b2271f
EduardoJaeger <dudufjaeger@gmail.com>
2014-10-23 03:02:38 -0200

    nao fnucoiadsihdf

--- GerEspFisico/src/main/java/clek/model/Course.java
+++ GerEspFisico/src/main/java/clek/model/Course.java
@@ -1,3 +1,5 @@
+package clek.model;
+
 import java.util.HashSet;
 
 
@@ -7,6 +9,10 @@ public class Course {
 	private String name;
 	private int module;
 	private HashSet<StudentsClass> classes;
+	
+	public Course(){
+		
+	}
 	
 	public Course(String code, int credit, String name, int module){
 		this.code = code;
@@ -20,16 +26,32 @@ public class Course {
 		return this.code;
 	}
 	
+	public void setCode(String code){
+		this.code = code;
+	}
+	
 	public int getCredit(){
 		return this.credit;
+	}
+	
+	public void setCredit(int credit){
+		this.credit = credit;
 	}
 	
 	public String getName(){
 		return this.name;
 	}
 	
+	public void setName(String name){
+		this.name = name;
+	}
+	
 	public int getModule(){
 		return this.module;
+	}
+	
+	public void setModule(int module){
+		this.module = module;
 	}
 	
 	public boolean addClass(StudentsClass c){
@@ -39,4 +61,15 @@ public class Course {
 	public HashSet<StudentsClass> getClasses(){
 		return this.classes;
 	}
+	
+	public void setClasses(HashSet<StudentsClass> classes){
+		this.classes = classes;
+	}
+	/*
+	public String toString(){
+		return new StringBuffer(" Code : ").append(this.code)
+				.append(" Credit : ").append(this.credit)
+				.append(" Name : ").append(this.name)
+				.append(" Module : ").append(this.module).toString();
+	}*/
 }
--- GerEspFisico/src/main/java/clek/model/Room.java
+++ GerEspFisico/src/main/java/clek/model/Room.java
@@ -7,6 +7,10 @@ public class Room {
 	private String roomName;
 	private HashMap<ClassTime, StudentsClass> classes;
 	
+	public Room (){
+		
+	}
+	
 	public Room (int capacity, String name){
 		this.capacity = capacity;
 		this.roomName = name;
@@ -17,12 +21,20 @@ public class Room {
 		return roomName;
 	}
 	
+	public void setRoomName(String roomName){
+		this.roomName = roomName;
+	}
+	
 	public StudentsClass addClass(ClassTime key, StudentsClass value){
 		return classes.put(key, value);
 	}
 	
 	public int getCapacity(){
 		return capacity;
+	}
+	
+	public void setCapacity(int capacity){
+		this.capacity = capacity;
 	}
 	
 	public HashMap<ClassTime, StudentsClass> geClasses(){
@@ -32,5 +44,11 @@ public class Room {
 	public void reset(){
 		this.classes = new HashMap<ClassTime, StudentsClass>();
 	}
+	/*
+	public String toString(){
+		return new StringBuffer(" Room Name : ").append(this.roomName)
+				.append(" Capacity : ").append(this.capacity)
+				.append(" Classes : ").append(this.classes.toString()).toString();
+	}*/
 	
 }
